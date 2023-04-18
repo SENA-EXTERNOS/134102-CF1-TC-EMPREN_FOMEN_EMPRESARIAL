f0f297737724c4f213abab9c5eb074e488402152
Andrea Botello <andrea.botellor@misena.edu.co>
2023-04-18 15:36:20 -0500

    Videos y material

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -58,7 +58,7 @@
     .row.mb-5
       figure.mb-5
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/OzXPBfhPdRI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     ul.lista-ul--color.mb-3
       li 
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
-  .curso-main-container.pb-3
-    BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
-    .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+.curso-main-container.pb-3
+  BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
   
          
       
-      p.mb-5 En el siguiente mapa conceptual podrá analizar cada uno de los contenidos temáticos del componente desarrollado anteriormente. 
-      .row.justify-content-center
-        .col-lg-10.mb-5
-          figure
-            img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
-        .col-auto
-          a.anexo.mb-4(:href="obtenerLink('/downloads/Sintesis-.pdf')" target="_blank")
-            .anexo__icono
-              img(src="@/assets/template/icono-pdf.svg")
-            .anexo__texto
-              p Anexo. Síntesis
+    p.mb-5 En el siguiente mapa conceptual podrá analizar cada uno de los contenidos temáticos del componente desarrollado anteriormente. 
+    .row.justify-content-center
+      .col-lg-10.mb-5
+        figure
+          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+      .col-auto
+        a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-pdf.svg")
+          .anexo__texto
+            p Anexo. Síntesis
 
 </template>
 
